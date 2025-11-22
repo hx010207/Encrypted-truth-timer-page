40bed174dabfe4a796552a98a0d44e5f362907b1
Harshit S jain <131752091+hx010207@users.noreply.github.com>
2025-11-22 17:53:52 +0530

    Remove background color from top right logo container

--- App.tsx
+++ App.tsx
@@ -135,7 +135,7 @@ const App: React.FC = () => {
                 </div>
             </div>
             <div className="absolute top-4 right-4 h-24 w-24 md:h-28 md:w-28 z-20">
-                <div className="p-2 bg-white/50 backdrop-blur-xl border-2 border-white/20 rounded-xl shadow-lg shadow-black/50 w-full h-full">
+                <div className="p-2 backdrop-blur-xl border-2 border-white/20 rounded-xl shadow-lg shadow-black/50 w-full h-full">
                     <img src="https://i.postimg.cc/MGfsxbB9/logo-jain-white.png" alt="Top Right Logo" className="w-full h-full object-contain"/>
                 </div>
             </div>
